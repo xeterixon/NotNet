8bef10d5cc19a970449bad25b7aa8bf788cc58ca
Xet Erixon <xet.erixon@gmail.com>
2017-02-13 19:51:53 +0100

    Fixed #3

--- NotNet.Core.Forms/NotNet.Core.Forms/Infrastructure/ApplicationDelegate.cs
+++ NotNet.Core.Forms/NotNet.Core.Forms/Infrastructure/ApplicationDelegate.cs
@@ -17,6 +17,7 @@ namespace NotNet.Core.Forms
 			app.ModalPushing += ModalPushing;
 			container.RegisterSingleton<INavigationLocator>(new NavigationLocator(this,container));
 		}
+
 		void Application_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
 		{
 			if ("MainPage" == e.PropertyName) {
@@ -26,12 +27,13 @@ namespace NotNet.Core.Forms
 					Navigations.Push(App.MainPage.Navigation);
 				}
 				//NOTE If navpage is null, other navigation will most likely not work
-				if (navpage != null) {
+				if (navpage != null)
+				{
 					navpage.Popped += NonModelPagePopped;
 				}
 			}
-
 		}
+
 		void NonModelPagePopped(object sender, NavigationEventArgs e)
 		{
 			var p = e.Page;
@@ -40,15 +42,28 @@ namespace NotNet.Core.Forms
 
 		void ModalPopped(object sender, ModalPoppedEventArgs e)
 		{
+			var navpage = e.Modal as NavigationPage;
+			if (navpage != null) 
+			{
+				navpage.Popped -= NonModelPagePopped;
+			}
 			e.Modal?.Cleanup();
-			if (Navigations.Count > 1 && e.Modal?.Navigation != null) {
+			if (Navigations.Count > 1 && e.Modal?.Navigation != null) 
+			{
 				Navigations.Pop();
 			}
-
 		}
+
 		void ModalPushing(object sender, ModalPushingEventArgs e)
 		{
-			if (e.Modal.Navigation != null) {
+			var navpage = e.Modal as NavigationPage;
+			if (navpage != null)
+			{
+				navpage.Popped += NonModelPagePopped;
+			}
+
+			if (e.Modal.Navigation != null) 
+			{
 				Navigations.Push(e.Modal.Navigation);
 			}
 		}
