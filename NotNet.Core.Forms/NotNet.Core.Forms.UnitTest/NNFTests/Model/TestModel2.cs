8eae1718cb56ece72aceaf92efbcc164a25e0b7d
Xet Erixon <xet.erixon@gmail.com>
2017-02-23 23:56:37 +0100

    Fixed Alert and ActionSheet
    Moved the code around as well.

--- NotNet.Core.Forms/NotNet.Core.Forms.UnitTest/NNFTests/Model/TestModel2.cs
+++ NotNet.Core.Forms/NotNet.Core.Forms.UnitTest/NNFTests/Model/TestModel2.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
 using NotNet.Core;
 using NotNet.Core.Forms;
 using Xamarin.Forms;
@@ -10,12 +11,37 @@ namespace NNFTests
  	{
 		public string Name { get; private set; }
 		public ICommand PopPageCommand { get; private set; }
+		public ICommand ShowAlertCommand { get; private set; }
+		public ICommand YesNoCommand { get; private set; }
+		public ICommand ShowPopupCommand { get; private set; }
 		INavigationLocator _navigation;
-		public TestModel2(INavigationLocator nav) 
+		IPopupService _popup;
+		public TestModel2(INavigationLocator nav, IPopupService popup) 
 		{
 			_navigation = nav;
+			_popup = popup;
 			Name = "Some Random Text";
 			PopPageCommand = new Command(async () => { await _navigation.PopAsync();});
+			ShowPopupCommand = new Command<string>(async (obj) => { await ShowPopup(obj);});
+		}
+		async Task ShowPopup(string type) 
+		{
+			if (type == "ALERT") 
+			{
+				await _popup.ShowAlert("Title", "Some message", "OK");
+			}
+			if (type == "QUESTION") 
+			{
+				var resp = await _popup.ShowAlert("Huh?", "Yes or no?", "Yes", "No");
+				var msg = resp ? "Yes, it is..." : "No, why not?";
+				await _popup.ShowAlert("Aha", msg, "OK");
+				
+			}
+			if (type == "SHEET") 
+			{
+				var resp = await _popup.ShowActionSheet("Options","Cancel","Delete","This","That", "The other" );
+				await _popup.ShowAlert("Choice", $"You picked {resp}", "OK");
+			}
 		}
 	}
 }
---
+++ NotNet.Core.Forms/NotNet.Core.Forms/Infrastructure/Popup/IPopupService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NotNet.Core.Forms
+{
+	public interface IPopupService
+	{
+#region Wrapped Page Tasks		
+		Task ShowAlert(string title, string message, string buttonText);
+		Task<bool> ShowAlert(string title, string message, string ok, string cancel);
+		Task<string> ShowActionSheet(string title, string cancel, string delete, params string[] others);
+#endregion
+
+	}
+}
