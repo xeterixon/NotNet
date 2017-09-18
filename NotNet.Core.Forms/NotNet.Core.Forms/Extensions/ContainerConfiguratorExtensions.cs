8e2d9bcc35aaddbba976d243c29d9dcfa7f62896
Xet Erixon <xet.erixon@gmail.com>
2017-09-18 07:55:12 +0200

    Removed obsolete stuff

--- NotNet.Core.Forms/NotNet.Core.Forms/Extensions/ContainerConfiguratorExtensions.cs
+++ NotNet.Core.Forms/NotNet.Core.Forms/Extensions/ContainerConfiguratorExtensions.cs
@@ -46,44 +46,5 @@ namespace NotNet.Core.Forms
 			return self.AddApplicationDelegate(app).AddNavigationLocator().AddPopupService().AddTimerService();
 		}
 
-		#region Obsolote stuff
-		public static IContainerConfiguration AddApplicationDelegate(this IContainerConfiguration self, Application app)
-		{
-			if (!self.Container.IsRegistered<IApplicationDelegate>())
-			{
-				self.RegisterSingleton<IApplicationDelegate>(new ApplicationDelegate(self.Container, app));
-			}
-			return self;
-		}
-		public static IContainerConfiguration AddNavigationLocator(this IContainerConfiguration self)
-		{
-
-			if (!self.Container.IsRegistered<INavigationLocator>())
-			{
-				self.Container.RegisterSingleton<INavigationLocator, NavigationLocator>();
-			}
-			return self;
-		}
-		public static IContainerConfiguration AddPopupService(this IContainerConfiguration self)
-		{
-			if (!self.Container.IsRegistered<IPopupService>())
-			{
-				self.Container.RegisterTransient<IPopupService, PopupService>();
-			}
-			return self;
-		}
-		public static IContainerConfiguration AddTimerService(this IContainerConfiguration self)
-		{
-			if (!self.Container.IsRegistered<ITimer>())
-			{
-				self.Container.RegisterSingleton<ITimer, Timer>();
-			}
-			return self;
-		}
-		public static IContainerConfiguration AddDefaultServices(this IContainerConfiguration self, Application app)
-		{
-			return self.AddApplicationDelegate(app).AddNavigationLocator().AddPopupService().AddTimerService();
-		}
-		#endregion
 	}
 }
